177c2959610a0652f7808227330ee1f986e43107
Phaedra <phaedra.a.r@gmail.com>
2017-11-13 23:27:03 -0800

    Unittest syntax

---
+++ setup.py
@@ -0,0 +1,6 @@
+setup_requires=['nose>=1.0']
+
+#setup (
+#    # ...
+#    test_suite = 'nose.collector'
+#)
--- tests/vehicle_sales/vehicle_sales_api_test.py
+++ tests/vehicle_sales/vehicle_sales_api_test.py
@@ -11,7 +11,6 @@ import unittest
 
 URL = 'http://localhost:8000/vehicle_sales/'
 HEADERS = {'Content-Type': 'application/json'}
-AUTH = ('admin', 'password123')
 
 
 VINS = [
@@ -49,6 +48,28 @@ CAR_CLASSES = [
 
 
 class VehicleSalesAPITests(unittest.TestCase):
+    def setUp(self):
+        self.auth = ('admin', 'password123')
+        data = {
+            'buyer': BUYERS[0],
+            'price': 989898.33,
+            'vin': VINS[0],
+            'car_class': CAR_CLASSES[0],
+            'seller': SELLERS[0],
+        }
+        # Not ideal solution. Better would be to get nose
+        # integration working.
+        res = requests.post(
+            URL, data=json.dumps(data), headers=HEADERS, auth=self.auth)
+        self.sale_id = res.json()['id']
+        self.detail_url = '{}{}'.format(URL, self.sale_id)
+        # self.created_sales = set()
+
+    #def tearDown(self):
+    #    for id in self.created_sales:
+    #        sale = VehicleSales.objects.get(id=id)
+    #        sale.delete()
+
     def test_vehicle_sales_post_invalid_buyer_seller_relationship(self):
         data = {
             'buyer': SELLERS[0],
@@ -58,14 +79,14 @@ class VehicleSalesAPITests(unittest.TestCase):
             'seller': SELLERS[0],
         }
         res = requests.post(
-            URL, data=json.dumps(data), headers=HEADERS, auth=AUTH)
-        assert res.status_code == 400
+            URL, data=json.dumps(data), headers=HEADERS, auth=self.auth)
+        self.assertEqual(res.status_code, 400)
 
         data['buyer'] = BUYERS[0]
         data['seller'] = BUYERS[0]
         res = requests.post(
-            URL, data=json.dumps(data), headers=HEADERS, auth=AUTH)
-        assert res.status_code == 400
+            URL, data=json.dumps(data), headers=HEADERS, auth=self.auth)
+        self.assertEqual(res.status_code, 400)
 
     def test_vehicle_sales_post_requests(self):
         price = 10000.00
@@ -79,31 +100,32 @@ class VehicleSalesAPITests(unittest.TestCase):
                 'seller': SELLERS[i]
             }
             res = requests.post(
-                URL, data=json.dumps(data), headers=HEADERS, auth=AUTH)
-            assert res.json()
-            assert res.status_code == 200
+                URL, data=json.dumps(data), headers=HEADERS, auth=self.auth)
+            self.assertTrue(res.json() != {})
+            self.assertEqual(res.status_code, 200)
 
             data = res.json()
-            assert data['id'] not in ids
+            self.assertTrue(data['id'] not in ids)
             ids.add(data['id'])
 
-            assert data['vin'] == VINS[i]
-            assert float(data['price']) == price
-            assert data['buyer_type'] == BUYERS[i]['type']
-            assert data['seller_type'] == SELLERS[i]['type']
+            self.assertEqual(data['vin'], VINS[i])
+            self.assertEqual(float(data['price']), price)
+            self.assertEqual(data['buyer_type'], BUYERS[i]['type'])
+            self.assertEqual(data['seller_type'], SELLERS[i]['type'])
 
             price += 12345.55
+        # self.created_sales = ids
 
-    def test_vehicle_sales_get_by_car_requests(self):
+    def test_vehicle_sales_car_detail(self):
         for vin in VINS:
             get_url = '{}car/{}'.format(URL, vin)
             res = requests.get(
                 get_url, data=json.dumps({'latest': True}),
-                headers=HEADERS, auth=AUTH)
-            assert res.json()
-            assert res.json().get('vin') == vin
+                headers=HEADERS, auth=self.auth)
+            self.assertTrue(res.json() != {})
+            self.assertEqual(res.json().get('vin'), vin)
 
-    def test_vehicle_sales_get_requests(self):
+    def test_vehicle_sales_list_get(self):
         ids = set()
         for i in range(len(BUYERS)):
             data = {
@@ -113,15 +135,29 @@ class VehicleSalesAPITests(unittest.TestCase):
                 'latest': True
             }
             res = requests.get(
-                URL, data=json.dumps(data), headers=HEADERS, auth=AUTH)
+                URL, data=json.dumps(data), headers=HEADERS, auth=self.auth)
 
             self.assertEqual(res.status_code, 200)
-            assert res.json()
+            self.assertTrue(res.json() != {})
 
             sale_id = res.json()['id']
-            assert sale_id not in ids
+            self.assertTrue(sale_id not in ids)
             ids.add(sale_id)
 
+    def test_vehicle_sales_detail_get(self):
+        res = requests.get(self.detail_url, headers=HEADERS, auth=self.auth)
+        self.assertEqual(res.json()['vin'], VINS[0])
+
+    def test_vehicle_sales_detail_put(self):
+        price = 99999.99
+        res = requests.put(
+            self.detail_url, data=json.dumps({'price': price}),
+            headers=HEADERS, auth=self.auth)
+        self.assertEqual(float(res.json()['price']), price)
+
+    def test_vehicle_sales_detail_delete(self):
+        res = requests.delete(self.detail_url, headers=HEADERS, auth=self.auth)
+        self.assertTrue(res.json().get('Success') is not None)
 
 if __name__ == '__main__':
     unittest.main()
